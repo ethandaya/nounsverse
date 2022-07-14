5b1e455496c44916af85accef4405100a6bad6f0
Ethan <me@ethandaya.com>
2022-07-14 15:29:20 +0200

    fix: gtag types

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -7,12 +7,47 @@ import { WagmiConfig } from "wagmi";
 import { chains, wagmiClient } from "../utils/network";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import NextNProgress from "nextjs-progressbar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <ThemeProvider defaultMode="dark" defaultAccent="yellow">
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <NextNProgress color={vars.colors.yellow} />
           <Component {...pageProps} />
         </ThemeProvider>
