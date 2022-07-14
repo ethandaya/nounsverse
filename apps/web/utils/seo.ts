f260d63fb10d040b8b7e4ab8860122c21b3dd7dc
Ethan <me@ethandaya.com>
2022-07-14 15:34:27 +0200

    chore: update site description

--- apps/web/utils/seo.ts
+++ apps/web/utils/seo.ts
@@ -1,4 +1,4 @@
 export const SITE_DESCRIPTION =
-  "Nounsverse.wtf is a data focused opensource nouns explorer";
+  "an opensource noun + noun derivative auction explorer";
 export const SITE_TITLE = "« nounsverse »";
 export const SITE_URL = "https://nounsverse.wtf";
